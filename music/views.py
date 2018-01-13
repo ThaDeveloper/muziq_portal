1f50734ea347369e3dda3cdfaa217f856fd49780
Justin Ndwiga <ndwigajustin@gmail.com>
2018-01-13 16:07:21 +0300

    [Features] Database,Admin and Paths

--- music/views.py
+++ music/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Album
 
 # Create your views here.
+#music index page
+def index(request):
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.album_title + '<a/><br>'
+    return HttpResponse(html)
+
+#single music page /music/1/
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id:" + str(album_id) + "</h2>")
+
