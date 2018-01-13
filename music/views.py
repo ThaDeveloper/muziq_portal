ca4b4abb38278d1613973245149854ccd28e596c
Justin Ndwiga <ndwigajustin@gmail.com>
2018-01-13 16:30:46 +0300

    [Feature] 404 error

--- music/views.py
+++ music/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 #from django.template import loader
 from .models import Album
+from django.http import Http404
 
 # Create your views here.
 #music index page
 def index(request):
     all_albums = Album.objects.all()
-    context = {'all_albums': all_albums }
-    return render(request, 'music/index.html', context)
+    #context = {'all_albums': all_albums }
+    return render(request, 'music/index.html', {'all_albums': all_albums })
 
 #single music page /music/1/
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id:" + str(album_id) + "</h2>")
-
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album not found")
+    return render(request, 'music/detail.html', {'album': album})
