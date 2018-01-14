6a5366a86fc3309904154c642af82c9ca0bed33c
Justin Ndwiga <ndwigajustin@gmail.com>
2018-01-14 20:19:11 +0300

    [Favorite] forms and song favoriting

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
     # index references /music/ url pattern
     url(r'^$', views.index, name = 'index'),
 
     # /music/712/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name = 'detail'),
+     # /music/album_id/favorite
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name = 'favorite'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
-#from django.http import HttpResponse
-#from django.template import loader
-from .models import Album
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Album,Song
+
 
 # Create your views here.
 #music index page
@@ -13,8 +11,19 @@ def index(request):
 
 #single music page /music/1/
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album not found")
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': 'You did not select a valid song',
+    } )
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
