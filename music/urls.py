f53d39103f95b3d516d183f7c3443c81e72ae4a8
Justin Ndwiga <ndwigajustin@gmail.com>
2018-01-22 20:14:27 +0300

    User registration and login

--- music/urls.py
+++ music/urls.py
@@ -6,6 +6,8 @@ app_name = 'music'
 urlpatterns = [
     # index references /music/ url pattern
     url(r'^$', views.IndexView.as_view(), name = 'index'),
+
+    url(r'^register$', views.UserFormView.as_view(), name = 'register'),
     # /music/712/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name = 'detail'),
     # music/album/add/
